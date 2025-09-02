b0e406f833d1bba1028c440f3ff7acb5d784b898
pythonic06 <rajrahul.0987654@gmail.com>
2025-09-03 01:41:31 +0530

    Update clone.groovy

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,4 +1,5 @@
 def call(){
-                echo 'this is for cloning the code'
-                git url: 'https://github.com/pythonic06/django-notes-app.git', branch:'main'
+  echo "this is for cloning the code"
+  echo 'test'
+  git url: "https://github.com/pythonic06/django-notes-app.git", branch:"main"
 }
